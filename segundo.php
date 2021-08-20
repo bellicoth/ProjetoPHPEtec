f2a9043f3c77ebd787ee5c525466a93e71838f5a
bellicoth <thiagobellico@outlook.com>
2021-08-19 22:12:13 -0300

    atualizando

---
+++ segundo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <?php 
+        #comentario
+        #data: 19/08/2021
+    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Testando Comentario</title>
+</head>
+<body>
+    <?php 
+     /*
+     Comentario de linha 
+     */
+    echo "Segundo exemplo de comentario";
+    $data = "19 de agosto de 2021";
+    ?>
+    <p>Este paragrafo esta sendo interpretado pelo html</p>
+    Arquivo criado em php <?php echo $data; ?>
+    
+</body>
+</html>
